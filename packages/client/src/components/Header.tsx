4e3c15adf099f6f70b8d951149ffef31f3e75b99
Jonathan <johnzballad@gmail.com>
2020-08-03 11:01:34 -0300

    fix(client): space between cards

--- packages/client/src/components/Header.tsx
+++ packages/client/src/components/Header.tsx
@@ -26,12 +26,14 @@ const Wrapper = styled.div`
 const Boxes = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
+  padding: 25px 0px;
+  width: 100%;
+  max-width: 600px;
+
   @media (max-width: 800px) {
     flex-direction: column;
     justify-content: center;
     align-items: center;
   }
-  justify-content: space-evenly;
-  padding: 25px 0px;
-  width: 100%;
 `;
